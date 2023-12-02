c99f53377d30d44789b91e7068a623f6d76b59b3
Verena Kopf <verena.kopf@outlook.com>
2023-12-02 22:46:33 +0100

    Add puzzle 2 for day 1

--- Day 1/Puzzle 1/script.py
+++ Day 1/Puzzle 1/script.py
@@ -1,6 +1,3 @@
-import os
-import re
-
 def get_first_digit(line):
     for char in line:
         if char.isdigit():
@@ -10,8 +7,8 @@ input = open('./input.txt')
 sum = 0
 for line in input.readlines():
     first_digit = get_first_digit(line)
-    second_digit = get_first_digit(line[::-1])
-    calibration_value = int(str(first_digit) + str(second_digit))
+    last_digit = get_first_digit(line[::-1])
+    calibration_value = int(str(first_digit) + str(last_digit))
     sum += calibration_value
 print(sum)
 input.close()
