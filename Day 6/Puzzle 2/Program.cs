101139dce2fa6b59f3814d458eca7db4f68fdbd6
Verena Kopf <verena.kopf@outlook.com>
2023-12-06 09:34:05 +0100

    Add puzzle 2 for day 6

---
+++ Day 6/Puzzle 2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+using Puzzle_2;
+
+var lines = File.ReadAllLines("input.txt").ToList();
+
+var regex = new Regex("\\d+");
+
+var time = double.Parse(string.Join("", regex.Matches(lines[0]).Select(match => match.Value)));
+var distance = double.Parse(string.Join("", regex.Matches(lines[1]).Select(match => match.Value)));
+
+var race = new Race(time, distance);
+
+var speedGainRange = race.GetSpeedGainRangeToWin();
+var rangeLength = speedGainRange.End - speedGainRange.Start + 1;
+
+Console.WriteLine(rangeLength);
