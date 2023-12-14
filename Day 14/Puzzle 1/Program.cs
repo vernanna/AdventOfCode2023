de2455bcc2463100aa05b885eb72768f6158b35c
Verena Kopf <verena.kopf@cssteam.at>
2023-12-14 13:47:24 +0100

    Add puzzle 1 for day 14

---
+++ Day 14/Puzzle 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using MoreLinq;
+
+var lines = File.ReadAllLines("input.txt").ToList();
+
+var result = lines.Transpose()
+    .SelectMany(column => GetWeights(column.ToList()))
+    .Sum();
+
+Console.WriteLine(result);
+
+IEnumerable<int> GetWeights(IReadOnlyCollection<char> spaces)
+{
+    var lastOccupiedSpace = -1;
+    foreach (var (index, space) in spaces.Index())
+    {
+        if (space == '.')
+        {
+            continue;
+        }
+        if (space == '#')
+        {
+            lastOccupiedSpace = index;
+        }else if (space == 'O')
+        {
+            lastOccupiedSpace += 1;
+            yield return spaces.Count - lastOccupiedSpace;
+        }
+    }
+}
+        
+        
