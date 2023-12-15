d7c91b157dc6d260ac9f44de9a914b2dfb60eaeb
Verena Kopf <verena.kopf@cssteam.at>
2023-12-15 07:50:06 +0100

    Add puzzle 1 for day 15

---
+++ Day 15/Puzzle 1/Program.cs
@@ -0,0 +1,6 @@
+﻿var sumOfHashes = string.Join("", File.ReadAllLines("input.txt"))
+    .Split(",")
+    .Select(step => step.Select(character => (int)character))
+    .Sum(step => step.Aggregate(0, (sum, s) => (sum + s) * 17 % 256));
+
+Console.WriteLine(sumOfHashes);
